9e0d5530b6c2ec294bbb6e95519b45d4ebd7adb0
Ben Alman <cowboy@rj3.net>
2014-01-16 16:52:12 -0500

    Basic setup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,76 @@
 
 module.exports = function(grunt) {
 
+  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     connect: {
-      options: {
-
+      dev: {
+        options: {
+          base: ['prod', 'src', '.'],
+          port: 8000,
+          hostname: '*',
+        }
       },
     },
+    clean: {
+      prod: 'prod',
+    },
     copy: {
-      options: {
-
+      app: {
+        src: 'src/pages/index.html',
+        dest: 'prod/index.html',
       },
     },
     jshint: {
-      options: {
-
+      build: {
+        options: {
+          jshintrc: '.jshintrc',
+        },
+        src: ['Gruntfile.js'],
+      },
+      app: {
+        options: {
+          jshintrc: 'src/.jshintrc',
+        },
+        src: ['src/**/*.js'],
       },
     },
     watch: {
-
+      livereload: {
+        options: {
+          livereload: true,
+        },
+        files: ['src/**/*.{js,css}', 'prod/*'],
+        tasks: [],
+      },
+      jshintrc: {
+        files: ['**/.jshintrc'],
+        tasks: ['jshint'],
+      },
+      scripts: {
+        files: ['<%= jshint.app.src %>'],
+        tasks: ['jshint:app'],
+      },
+      page: {
+        files: 'src/pages/*.html',
+        tasks: ['copy:app'],
+      },
     },
   });
 
+  // Load Grunt plugins.
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Tasks.
+  grunt.registerTask('dev',
+    'Compile and start a dev webserver.',
+    ['jshint', 'clean:prod', 'copy:app', 'connect:dev', 'watch']);
+
+  grunt.registerTask('default', ['dev']);
 
 };
---
+++ src/main.js
@@ -0,0 +1,6 @@
+
+function awesome() {
+  document.body.innerHTML = 'OMG';
+}
+
+awesome();
