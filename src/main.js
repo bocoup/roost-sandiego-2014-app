8a6fd47f53057fb0a17e1910bf8ac7b8bc7e0d52
Irene Ros <imirene@gmail.com>
2014-01-17 12:42:35 -0500

    adding photo view

--- src/main.js
+++ src/main.js
@@ -11,16 +11,32 @@ var Photo = Backbone.Model.extend({
   }
 });
 
+var PhotoView = Backbone.View.extend({
+  model : Photo,
+
+  render: function() {
+
+    // add to the DOM:
+    $('<img>', {
+      src: this.model.get("dataUri")
+    }).appendTo(this.$el);
+
+    return this;
+  }
+});
+
 var me = new Photo({
   id: 1
 });
 
+var view = new PhotoView({
+  model : me
+});
+
+view.$el.appendTo('body');
+
 me.fetch().then(function(photo) {
   console.log(photo);
 
-  // add to the DOM:
-  $('<img>', {
-    src: photo.dataUri
-  }).appendTo('body');
-
+  view.render();
 });
