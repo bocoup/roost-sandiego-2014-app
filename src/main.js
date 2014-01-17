4a9e9e041db9571c823ca4c410fdc5feb24bccfa
Irene Ros <imirene@gmail.com>
2014-01-17 12:33:02 -0500

    Adding photo model

--- src/main.js
+++ src/main.js
@@ -4,3 +4,23 @@ function awesome() {
 }
 
 awesome();
+
+var Photo = Backbone.Model.extend({
+  urlRoot : function() {
+    return "http://localhost:8001/photos";
+  }
+});
+
+var me = new Photo({
+  id: 1
+});
+
+me.fetch().then(function(photo) {
+  console.log(photo);
+
+  // add to the DOM:
+  $('<img>', {
+    src: photo.dataUri
+  }).appendTo('body');
+
+});
