da58734b13d13d77429042667582a8940cf45525
Mike Pennisi <mike@mikepennisi.com>
2014-01-31 14:18:12 -0500

    Reduce repetition in tests with Mocha `setup` hook

--- test/unit/tests/base-view.js
+++ test/unit/tests/base-view.js
@@ -2,43 +2,45 @@ define(['src/modules/core/base-view'], function(BaseView) {
   'use strict';
 
   suite('BaseView', function() {
+
+    setup(function() {
+      this.view = new BaseView();
+    });
+
     suite('#render', function() {
       test('invokes custom `postRender` hook', function() {
-        var view = new BaseView();
         var invoked = false;
-        view.postRender = function() {
+        this.view.postRender = function() {
           invoked = true;
         };
-        view.template = function() {};
+        this.view.template = function() {};
 
-        view.render();
+        this.view.render();
 
         assert.ok(invoked);
       });
 
       test('correctly sets the markup to be used in BaseView#place', function() {
-        var view = new BaseView();
-        view.template = function() {
+        this.view.template = function() {
           return '<h1>test!</h1>';
         };
 
-        view.render();
-        view.place();
+        this.view.render();
+        this.view.place();
 
-        assert.equal(view.$el.html(), '<h1>test!</h1>');
+        assert.equal(this.view.$el.html(), '<h1>test!</h1>');
       });
 
       test('expands the template with the data returned by `serializeData`', function() {
-        var view = new BaseView();
         var expectedData = {};
         var actualData;
-        view.serializeData = function() {
+        this.view.serializeData = function() {
           return expectedData;
         };
-        view.template = function(data) {
+        this.view.template = function(data) {
           actualData = data;
         };
-        view.render();
+        this.view.render();
 
         assert.equal(actualData, expectedData);
       });
@@ -46,12 +48,11 @@ define(['src/modules/core/base-view'], function(BaseView) {
 
     suite('#place', function() {
       test('invokes custom `postPlace` hook', function() {
-        var view = new BaseView();
         var invoked = false;
-        view.postPlace = function() {
+        this.view.postPlace = function() {
           invoked = true;
         };
-        view.place();
+        this.view.place();
 
         assert.ok(invoked);
       });
