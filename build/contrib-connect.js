d707fc5339d082931a49cd6a491b0ea519f367ab
Ben Alman <cowboy@rj3.net>
2014-01-31 17:42:30 -0500

    Make connect server pushState-friendly.

--- build/contrib-connect.js
+++ build/contrib-connect.js
@@ -1,9 +1,31 @@
 module.exports = function(grunt) {
 
+  // If the request URL matches any of the app's routes, rewrite
+  // the URL to be /index.html. On the final production server,
+  // this will most likely be done with Nginx/Apache rewrites.
+  var root = '/index.html';
+  function rewriteRoute(req, res, next) {
+    if (/^\/(?:webcam|upload|photo\/[^\/]+)$/.test(req.url)) {
+      grunt.log.debug('PUSHSTATE ' + req.url + ' -> ' + root);
+      req.url = root;
+    }
+    next();
+  }
+
   grunt.config.set('connect', {
     options: {
       port: 8000,
       hostname: '*',
+      middleware: function(connect, options) {
+        var middleware = [];
+        // Rewrite routes as-necessary.
+        middleware.push(rewriteRoute);
+        // Serve static files.
+        options.base.forEach(function(base) {
+          middleware.push(connect.static(base));
+        });
+        return middleware;
+      },
     },
     dev: {
       options: {
