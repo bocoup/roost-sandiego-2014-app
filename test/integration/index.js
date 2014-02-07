df5ce24bb555335985c9197f5bbcdb484186bbc6
Mike Pennisi <mike@mikepennisi.com>
2014-02-07 12:16:29 -0500

    Reset photo database for each integration test
    Avoiding shared state helps prevent accidental inter-test dependencies.
    This property promotes test extensibility.

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,27 +1,68 @@
+var fs = require('fs');
+
 var webdriver = require('selenium-webdriver');
 var assert = require('assert');
 var path = require('path');
 var chromeDriver = require('selenium-chromedriver');
 var chrome = require('selenium-webdriver/chrome');
 var chromeService = new chrome.ServiceBuilder(chromeDriver.path);
+var db = {
+  dir: path.join(__dirname, '..', '..', 'server')
+};
+db.file = path.join(db.dir, 'images.db');
+db.backup = path.join(db.dir, 'images-backup.db');
 
 suite('Roostagram', function() {
   var driver;
-  this.timeout(8000);
+  this.timeout(15000);
+
+  // Save the current state of the photos database to a temporary file
+  suiteSetup(function() {
+    fs.renameSync(db.file, db.backup);
+  });
+  // Restore the original photos database when the tests are complete.
+  // Windows sometimes maintains a lingering reference to the database file
+  // after the tests complete, preventing backup restoration. Retry for a
+  // limited amount of time.
+  suiteTeardown(function(done) {
+    var start = Date.now();
+    function tryCleanup() {
+      if (Date.now() - start > 5000) {
+        done(new Error('Unable to restore backup database file.'));
+        return;
+      }
+      try {
+        fs.renameSync(db.backup, db.file);
+        done();
+      } catch(err) {
+        setTimeout(tryCleanup, 200);
+      }
+    }
+
+    tryCleanup();
+  });
 
   setup(function(done) {
-    driver = chrome.createDriver(null, chromeService.build());
-    driver.get('http://localhost:8000');
-    driver.manage().timeouts().implicitlyWait(3000);
-    driver.wait(function() {
-      return driver.isElementPresent(webdriver.By.css('.container'));
-    }, 3000).then(function() {
-      return driver.findElements(webdriver.By.css('.thumbnail'));
-    }).then(function(thumbnails) {
-      this.initThumbCount = thumbnails.length;
-      done();
+    // Initialize the state of the photos database before every test.
+    var origDb = fs.createReadStream(db.backup);
+    var tempDb = fs.createWriteStream(db.file);
+    origDb.pipe(tempDb);
+    origDb.on('end', function() {
+
+      driver = chrome.createDriver(null, chromeService.build());
+      driver.get('http://localhost:8000');
+      driver.manage().timeouts().implicitlyWait(3000);
+      driver.wait(function() {
+        return driver.isElementPresent(webdriver.By.css('.container'));
+      }, 3000).then(function() {
+        return driver.findElements(webdriver.By.css('.thumbnail'));
+      }).then(function(thumbnails) {
+        this.initThumbCount = thumbnails.length;
+        done();
+      }.bind(this));
     }.bind(this));
   });
+
   teardown(function() {
     driver.quit();
   });
